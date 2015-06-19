77ef55d9939deea12f59890a5053111003ac88a5
wang <zpkx.wang@gmail.com>
2015-06-20 01:29:13 +0200

    change ignore rule to add .classpath and add NetworkDownloader in Algorithms.io

---
+++ Algorithms/src/io/NetworkDownloader.java
@@ -0,0 +1,45 @@
+package io;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class NetworkDownloader {
+
+	public static void main(String[] args) {
+		String BASE = "https://d396qusza40orc.cloudfront.net"
+					+ "/ml/docs/slides/Lecture";
+		String EXTENSION = ".pdf";
+		String FOLDER = "src/io/download/";
+		
+		File dir = new File(FOLDER);
+		try {
+			if (!dir.exists())
+				Files.createDirectory(dir.toPath());
+			else
+				return;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		for(int i=1; i<=18; i++) {
+			URL url = null;
+			try {
+				url = new URL( BASE + i + EXTENSION);
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+			}
+			
+			try( InputStream is = url.openStream()) {
+				Files.copy( is, Paths.get( FOLDER + i + EXTENSION));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
